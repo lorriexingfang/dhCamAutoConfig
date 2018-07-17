d8d68c370968509dfef3a25f2715da4aa991a8e3
xfang <lorriexingfang@gmail.com>
2018-07-17 11:54:06 -0700

    variablize some parameters

--- set_params.js
+++ set_params.js
@@ -2,7 +2,13 @@ var request = require("request"),
     crypto = require('crypto');
 
 var username = 'admin',
-    password = 'abc123';
+    password = 'abc123',
+    realm = 'Login to 3L08942PAA8AECE',
+    http_method = 'GET',
+    uri = '/cgi-bin/configManager.cgi?action=setConfig&VideoColor[0][0].Brightness=60&VideoColor[0][0].Contrast=50&VideoColor[0][0].Saturation=60&VideoColor[0][0].Gamma=55',
+    qop = 'auth',
+    nc = '00000001',
+    cnonce = '0a4f113b';
 
 function firstRequest(callback){
     var options = { 
@@ -40,9 +46,14 @@ firstRequest(function(data, error){
     console.log("data is: ", data);
 
     /*Get response based on first http request*/
-    var ha1 = crypto.createHash('md5').update(username + ':' + 'Login to 3L08942PAA8AECE' + ':' + password).digest('hex');
-    var ha2 = crypto.createHash('md5').update('GET:' + '/cgi-bin/configManager.cgi?action=setConfig&VideoColor[0][0].Brightness=60&VideoColor[0][0].Contrast=50&VideoColor[0][0].Saturation=60&VideoColor[0][0].Gamma=55').digest('hex');
-    var response = crypto.createHash('md5').update(ha1 + ':' + data + ':00000001:0a4f113b:auth:' + ha2).digest('hex');
+    var ha1 = crypto.createHash('md5').update(username + ':' + realm + ':' + password).digest('hex');
+    var ha2 = crypto.createHash('md5').update(http_method + ':' + uri).digest('hex');
+    var response = crypto.createHash('md5').update(ha1 + ':' +
+                                                   data + ':' + 
+                                                   nc + ':' + 
+                                                   cnonce + ':' + 
+                                                   qop + ':' + 
+                                                   ha2).digest('hex');
 
     var auth_string = 'Digest username="admin", realm="Login to 3L08942PAA8AECE", nonce="' + data + '", uri="/cgi-bin/configManager.cgi?action=setConfig&VideoColor[0][0].Brightness=60&VideoColor[0][0].Contrast=50&VideoColor[0][0].Saturation=60&VideoColor[0][0].Gamma=55", algorithm="MD5", qop=auth, nc=00000001, cnonce="0a4f113b", opaque="3fd83f11d2cf03b2cef8e4555820db51cf4d3935", response="' + response + '"';
 
@@ -69,5 +80,7 @@ firstRequest(function(data, error){
 
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
+
+      //console.log(" 2nd response is: ", response);
     });  
 });
